6e5be36ccc5047009a43f06b9de9835c8efd3748
beybora <bora.b.sarikaya@gmail.com>
2024-10-29 12:57:00 +0100

    load dummy data into dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController
 {
-    public function index() {
-        return view('dashboard');
+    public function index()
+    {
+        $idea = new Idea([
+            'content' => 'Dummy content, this is a dummy content, this is what it is - a dummy content.',
+        ]);
+        $idea->save();
+
+        dump(Idea::all());
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at','DESC')->get(),
+        ]);
     }
 }
