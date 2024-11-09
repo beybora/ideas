0be13cce927b8c5bc4e559d62914339dae0ba65f
beybora <bora.b.sarikaya@gmail.com>
2024-11-09 15:14:03 +0100

    add routes and give given routes new alias

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\TermsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',  [DashboardController::class, 'index']);
+Route::get('/',  [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
 
 Route::get('/terms', [TermsController::class, 'index']);
 
+Route::post('/idea', [IdeaController::class, 'store']) ->name("idea.create");
