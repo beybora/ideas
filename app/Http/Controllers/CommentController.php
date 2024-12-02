cde450e9f379148234e4f4ffbd8206b89ad10a10
beybora <bora.b.sarikaya@gmail.com>
2024-12-02 20:59:33 +0100

    add store method to CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class CommentController
 {
-    //
+    public function store(Idea $idea)
+    {
+        $comment = new Comment();
+        $comment->idea_id = $idea->id;
+        $comment->content = request()->get('content');
+        $comment->save();
+
+        return redirect()
+            ->route('ideas.show', $idea->id)
+            ->with('success', 'Comment was added');
+    }
 }
