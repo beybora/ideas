ee28bbf869c8fb173091444b5e2e76e765d2e5b5
beybora <bora.b.sarikaya@gmail.com>
2024-12-02 20:33:12 +0100

    register route for creating comments

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\TermsController;
@@ -20,3 +21,5 @@ Route::get(('/ideas/{idea}'), [IdeaController::class, 'show'])->name('ideas.show
 Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
 
 Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update');
+
+Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store');
