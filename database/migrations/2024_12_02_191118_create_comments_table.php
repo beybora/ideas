ee2c702720e942133d016fa7c5895ae97a78a959
beybora <bora.b.sarikaya@gmail.com>
2024-12-02 20:33:34 +0100

    adjust comments migration

--- database/migrations/2024_12_02_191118_create_comments_table.php
+++ database/migrations/2024_12_02_191118_create_comments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,10 +12,11 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('idea_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+            $table->string('content');
         });
     }
-
     /**
      * Reverse the migrations.
      */
