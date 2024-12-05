53e5cb4307b6be714d1decd4e82769a0581c361b
beybora <bora.b.sarikaya@gmail.com>
2024-12-05 10:10:22 +0100

    add @auth and @guest

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.create');
 
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
-Route::get(('/ideas/{idea}'), [IdeaController::class, 'show'])->name('ideas.show');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
 
 Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit');
 
@@ -26,3 +26,9 @@ Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 
 Route::post('/register', [AuthController::class, 'store']);
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+Route::post('/login', [AuthController::class, 'authenticate']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
