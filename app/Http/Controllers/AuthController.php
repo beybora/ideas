79d86f350f0c5d573f4de8d52a273fb9274f96e8
beybora <bora.b.sarikaya@gmail.com>
2024-12-05 10:10:52 +0100

    add login functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
@@ -25,9 +28,40 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $validated['name'],
             'email' => $validated['email'],
-            'password' =>$validated['password']
+            'password' => Hash::make($validated['password']),
         ]);
 
         return redirect()->route('dashboard')->with('success', 'User created successfully!');
+    }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate()
+    {
+        $validated = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt($validated)) {
+            request()->session()->regenerate();
+            return redirect()->route('dashboard')->with('success', 'User logged in successfully!');
+        }
+
+        return redirect()
+            ->route('login')
+            ->withErrors(['email' => 'No matching user found with the provided email and password!']);
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        request()->session()->invalidate();
+        request()->sessioN()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('sucess', 'User logged out successfully!');
     }
 }
