b2d1eb457e190c27ee9ab526d5bfbfaf98b80882
beybora <bora.b.sarikaya@gmail.com>
2024-12-11 10:55:09 +0100

    group routes together with 'middleware => auth'

--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,25 @@ Route::group(['prefix' => 'ideas/', 'as' => 'ideas.'], function () {
 
     Route::get('{idea}', [IdeaController::class, 'show'])->name('show');
 
-    Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit')->middleware('auth');
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
 
-    Route::put('{idea}', [IdeaController::class, 'update'])->name('update')->middleware('auth');
+        Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
 
-    Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy')->middleware('auth');
+        Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
 
-    Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store')->middleware('auth');
+        Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+    });
+
+    Route::get('/terms', [TermsController::class, 'index']);
+
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+    Route::post('/register', [AuthController::class, 'store']);
+
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+    Route::post('/login', [AuthController::class, 'authenticate']);
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
-
-Route::get('/terms', [TermsController::class, 'index']);
-
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-Route::post('/register', [AuthController::class, 'store']);
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
