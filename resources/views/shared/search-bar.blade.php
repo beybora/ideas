d844cf4c51c3d050ad6c9060c8891180fc7cc3cd
beybora <bora.b.sarikaya@gmail.com>
2024-12-12 09:12:37 +0100

    bind search param to input field as a value

--- resources/views/shared/search-bar.blade.php
+++ resources/views/shared/search-bar.blade.php
@@ -4,10 +4,9 @@
     </div>
     <div class="card-body">
         <form action="{{ route('dashboard') }}" method="GET">
-        <input placeholder="...
-        " class="form-control w-100" type="text"
-            name="search">
-        <button class="btn btn-dark mt-2"> Search</button>
+            <input placeholder="...
+        " value={{ request('search', '') }}class="form-control w-100" type="text" name="search">
+            <button class="btn btn-dark mt-2"> Search</button>
         </form>
     </div>
 </div>
